2d772ecaa6f42da29c283355c0e85cbf1ab6b3ad
harshit Mishra <miananya2@gmail.com>
2024-07-11 01:18:39 +0530

    createSettingsSlice

--- store/settingsSlice.ts
+++ store/settingsSlice.ts
@@ -1,7 +1,12 @@
-import { Settings } from "@/types/SettingsTypes";
+import {
+  FormWithBulletPoints,
+  GeneralSettings,
+  SettingsState,
+  ShowForm,
+} from "@/types/SettingsTypes";
 import { StateCreator } from "zustand";
 
-export const initialSettings: Settings = {
+export const initialSettings: SettingsState = {
   themeColor: "#38dbdf8",
   fontFamily: "Roboto",
   fontSize: "11",
@@ -28,3 +33,67 @@ export const initialSettings: Settings = {
     custom: true,
   },
 };
+export type SettingsAction = {
+  changeSettings: (field: GeneralSettings, value: string) => void;
+  changeShowForm: (form: ShowForm, value: boolean) => void;
+  changeFormHeading: (form: ShowForm, value: string) => void;
+  changeFormOrder: (form: ShowForm, type: "up" | "down") => void;
+  changeBulletPoints: (form: FormWithBulletPoints, value: boolean) => void;
+  setSetting: (settings: SettingsState) => void;
+};
+
+export type settingsSliceType = SettingsState & SettingsAction;
+
+export const createSettingsSlice: StateCreator<
+  settingsSliceType,
+  [],
+  [],
+  settingsSliceType
+> = (set) => ({
+  ...initialSettings,
+  changeSettings: (field, value) =>
+    set((state) => ({
+      ...state,
+      [field]: value,
+    })),
+  changeShowForm: (form, value) =>
+    set((state) => ({
+      ...state,
+      formToShow: {
+        ...state.formToShow,
+        [form]: value,
+      },
+    })),
+  changeFormHeading: (form, value) =>
+    set((state) => ({
+      ...state,
+      formToHeading: {
+        ...state.formToHeading,
+        [form]: value,
+      },
+    })),
+  changeFormOrder: (form, type) =>
+    set((state) => {
+      const lastIndex = state.formsOrder.length - 1;
+      const position = state.formsOrder.indexOf(form);
+      const newPosition = type === "up" ? position - 1 : position + 1;
+      if (position >= 0 && newPosition <= lastIndex) {
+        const newFormsOrder = [...state.formsOrder];
+        [newFormsOrder[position], newFormsOrder[newPosition]] = [
+          newFormsOrder[newPosition],
+          newFormsOrder[position],
+        ];
+        return { ...state, formsOrder: newFormsOrder };
+      }
+      return state;
+    }),
+  changeBulletPoints: (form, value) =>
+    set((state) => ({
+      ...state,
+      showBulletsPoints: {
+        ...state.showBulletsPoints,
+        [form]: value,
+      },
+    })),
+  setSetting: (settings) => set((state) => ({ ...settings })),
+});
--- types/SettingsTypes/index.ts
+++ types/SettingsTypes/index.ts
@@ -1,44 +1,34 @@
+export type SettingsState = {
+  themeColor: string;
+  fontFamily: string;
+  fontSize: string;
+  documentSize: string;
+  formToShow: {
+    workExperience: boolean;
+    education: boolean;
+    projects: boolean;
+    skills: boolean;
+    custom: boolean;
+  };
+  formToHeading: {
+    workExperience: string;
+    education: string;
+    projects: string;
+    skills: string;
+    custom: string;
+  };
+  formsOrder: ShowForm[];
+  showBulletsPoints: {
+    education: boolean;
+    projects: boolean;
+    skills: boolean;
+    custom: boolean;
+  };
+};
 
-export type Settings = {
-    themeColor: string;
-    fontFamily: string;
-    fontSize: string;
-    documentSize: string;
-    formToShow: {
-      workExperience: boolean;
-      education: boolean;
-      projects: boolean;
-      skills: boolean;
-      custom: boolean;
-    };
-    formToHeading: {
-      workExperience: string;
-      education: string;
-      projects: string;
-      skills: string;
-      custom: string;
-    };
-    formsOrder: ShowForm[];
-    showBulletsPoints: {
-      education: boolean;
-      projects: boolean;
-      skills: boolean;
-      custom: boolean;
-    };
-  };
-  
-  export type ShowForm = keyof Settings["formToShow"];
-  export type FormWithBulletPoints = keyof Settings["showBulletsPoints"];
-  export type GeneralSettings = Exclude<
-    keyof Settings,
-    "formToShow" | "formToHeading" | "formsOrder" | "showBulletsPoints"
-  >;
-  
-  export type SettingsAction = {
-    changeSettings: (field: GeneralSettings, value: string) => void;
-    changeShowForm: (form: ShowForm, value: boolean) => void;
-    changeFormHeading: (form: ShowForm, value: string) => void;
-    changeFormOrder: (form: ShowForm, type: "up" | "down") => void;
-    changeBulletPoints: (form: FormWithBulletPoints, value: boolean) => void;
-    setSetting: (settings: Settings) => void;
-  };
+export type ShowForm = keyof SettingsState["formToShow"];
+export type FormWithBulletPoints = keyof SettingsState["showBulletsPoints"];
+export type GeneralSettings = Exclude<
+  keyof SettingsState,
+  "formToShow" | "formToHeading" | "formsOrder" | "showBulletsPoints"
+>;
